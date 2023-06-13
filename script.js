1a3cbcb634dc1d6a5c2915f93acec8fe617c021c
David Proto <davidproto@sbcglobal.net>
2023-06-13 00:22:51 -0400

    made price and product pages

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("price").addEventListener("click", function () {
+    window.location.href = "price.html";
+})
+document.getElementById("about").addEventListener("click", function () {
+    window.location.href = "about.html";
+})
